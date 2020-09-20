06ab9a623ff58269a1b0aa1d66370ab184dafd01
Ibtid <ibtid011@example.com>
2020-09-21 02:32:34 +0600

    header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Online Shop</h1>
+      <p>The safest place to shop</p>
+      <div>
+        <button type="button">Get Started</button>
+      </div>
+      <img src="../images/online-shop.png" />
+    </header>
+  );
+};
+
+export default Header;
