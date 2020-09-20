60b7c91e5ec210d28a1641f51c3df74443b3078c
Ibtid <ibtid011@example.com>
2020-09-21 04:39:21 +0600

    backbone

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container">
-      <Header />
+      <nav class="navbar">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#service">Service</a>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <section id="home">
+        <h1>Welcome To My Site</h1>
+        <p>Get Started</p>
+      </section>
+      <section id="about">
+        <h1>About</h1>
+        <p>Get Started</p>
+      </section>
+      <section id="service">
+        <h1>Service</h1>
+        <p>Get Started</p>
+      </section>
+      <section id="contact">
+        <h1>Contact</h1>
+        <p>Get Started</p>
+      </section>
     </div>
   );
 }
