4d7a028c69546925be71fd6bc576f3295fea2d24
Ibtid <ibtid011@example.com>
2020-09-21 10:30:28 +0600

    refactor

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import React from "react";
+import LandingPage from "./LandingPage/LandingPage";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="container">
-      <nav class="navbar">
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#service">Service</a>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <section id="home">
-        <h1>Welcome To My Site</h1>
-        <p>Get Started</p>
-        <button className="login" type="button">
-          Login
-        </button>
-      </section>
-      <section id="about">
-        <h1>About</h1>
-        <p>Get Started</p>
-      </section>
-      <section id="service">
-        <h1>Service</h1>
-        <p>Get Started</p>
-      </section>
-      <section id="contact">
-        <h1>Contact</h1>
-        <p>Get Started</p>
-      </section>
-    </div>
-  );
+  return <LandingPage />;
 }
 
 export default App;
---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="container">
+      <nav class="navbar">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#service">Service</a>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <section id="home">
+        <h1>Welcome To My Site</h1>
+        <p>Get Started</p>
+        <button className="login" type="button">
+          Login
+        </button>
+      </section>
+      <section id="about">
+        <h1>About</h1>
+        <p>Get Started</p>
+      </section>
+      <section id="service">
+        <h1>Service</h1>
+        <p>Get Started</p>
+      </section>
+      <section id="contact">
+        <h1>Contact</h1>
+        <p>Get Started</p>
+      </section>
+    </div>
+  );
+};
+
+export default LandingPage;
