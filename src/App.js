082fec962bb51f89c2ba4592359927e83182cf2c
Ibtid <ibtid011@example.com>
2020-09-22 19:09:36 +0600

    added react router dom

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import LandingPage from "./LandingPage/LandingPage";
 import "./App.css";
 import LoginForm from "./Login/components/LoginForm";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <LoginForm />;
+  return (
+    <Router>
+      <Route path="/" exact component={LandingPage} />
+      <Route path="/LoginForm" component={LoginForm} />
+    </Router>
+  );
 }
 
 export default App;
