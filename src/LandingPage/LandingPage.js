a47f4a50183885cdd7678e05f3b1fdf6c1136c65
Ibtid <ibtid011@example.com>
2020-09-23 10:50:29 +0600

    new landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,38 +4,18 @@ import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div className="container">
-      <nav class="navbar">
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#service">Service</a>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <section id="home">
-        <h1>Welcome To My Site</h1>
-        <p>Get Started</p>
-        <Link to="/LoginForm">
-          <button className="LandingPageLogin" type="button">
-            Login
-          </button>
-        </Link>
-      </section>
-      <section id="about">
-        <h1>About</h1>
-        <p>Get Started</p>
-      </section>
-      <section id="service">
-        <h1>Service</h1>
-        <p>Get Started</p>
-      </section>
-      <section id="contact">
-        <h1>Contact</h1>
-        <p>Get Started</p>
-      </section>
+    <div className="LandingMain">
+      <div className="Landingbox1"></div>
+      <div className="Landingbox2">
+        <div className="Landingtext">
+          Extreme <br /> UI DESIGN
+        </div>
+        <div className="LandingButton">
+          <Link to="/LoginForm">
+            <button className="LandingGetStartedButton">Get Started</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
